14a69101af39b3502b46bbfd5ceba352bbe0e5ca
niklhut <49069026+niklhut@users.noreply.github.com>
2025-12-14 10:39:09 +0100

    feat: prepare for release

---
+++ src/main/java/h10/AirEnclosure.java
@@ -0,0 +1,27 @@
+package h10;
+
+import h10.abilities.Flies;
+import h10.animals.Animal;
+import org.tudalgo.algoutils.student.annotation.StudentImplementationRequired;
+
+// TODO: H10.2.2
+public class AirEnclosure<TODO_REPLACE> {
+
+    @StudentImplementationRequired("H10.2.2")
+    // TODO: H10.2.2
+    final StackOfObjects animals = null;
+
+    @StudentImplementationRequired("H10.2.2")
+    @Override
+    public StackOfObjects getAnimals() {
+        // TODO: H10.2.2
+        return org.tudalgo.algoutils.student.Student.crash("H10.2.2 - Remove if implemented");
+    }
+
+    @StudentImplementationRequired("H10.2.2")
+    @Override
+    public void feed() {
+        // TODO: H10.2.2
+        org.tudalgo.algoutils.student.Student.crash("H10.2.2 - Remove if implemented");
+    }
+}
---
+++ src/test/java/h10/EnclosureTest.java
@@ -0,0 +1,36 @@
+package h10;
+
+import h10.animals.birds.Penguin;
+import h10.animals.mammals.JugglingLion;
+import h10.animals.mammals.Lion;
+import org.junit.jupiter.api.Test;
+import org.tudalgo.algoutils.student.annotation.StudentImplementationRequired;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * An example JUnit test class.
+ */
+public class EnclosureTest {
+
+    @Test
+    @StudentImplementationRequired("H10.5.1")
+    public void testAnyMatch() {
+        // TODO: H10.5.1
+        org.tudalgo.algoutils.student.Student.crash("H10.5.1 - Remove if implemented");
+    }
+
+
+    @Test
+    @StudentImplementationRequired("H10.5.2")
+    public void testMapFunctional() {
+        // Here's an example for an expected sign after all transformations for 3 penguins
+        String exampleSign = "Adult Penguin Pool: " +
+            "\nSir Pip (3 years old)" +
+            "\nSir Penny (4 years old)" +
+            "\nSir Percy (2 years old)";
+
+        // TODO: H10.5.2
+        org.tudalgo.algoutils.student.Student.crash("H10.5.2 - Remove if implemented");
+    }
+}
